7bd1400e94b989e503a76ed3721ad6b01adacb09
EQE lab <eqelab@gmail.com>
2026-04-13 11:09:33 +0200

    imgs

--- run_experiment_fiber_coupling.py
+++ run_experiment_fiber_coupling.py
@@ -8,7 +8,7 @@ from controller.servos import Servos
 def run_experiment(params, picoscope):
     yaw_1, yaw_2, pitch_2, pitch_1 = params
     sts_goal_positions = [
-        math.floor(yaw_1),
+        math.floor(yaw_1), #TEST round()
         math.floor(yaw_2),
         math.floor(pitch_2),
         math.floor(pitch_1), #[2654, 515, 1094, 921]
